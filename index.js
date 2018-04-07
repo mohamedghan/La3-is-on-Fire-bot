bc87d58ecf29dc3d98fd6c518aedd435235a897f
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-04-08 00:13:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const prefix = "3! ";
+const prefix = "!";
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
   bot.user.setActivity("my skills", { type: 'TESTING' });
@@ -10,24 +10,23 @@ bot.on('ready', () => {
 
 
 
-
 //commands
 bot.on('message', msg => {
-var cha = msg.guild.channels.find('name', 'bot_commands').id;
-if (msg.content.startsWith('!p ') || msg.content.startsWith('!play ') || msg.content.startsWith('!s ') || msg.content.startsWith('!skip ') || msg.content.startsWith('!leave ') || msg.content.startsWith('!join ')) {
-	if(cha != msg.channel.id) {
-		msg.reply('bellehi sayeb sala7 wel commands ektebhom ken fel bot_commands. ani sama7tek el marra hethi ama el marra ejjeya wlh n9oul el hama yetfehem m3ak');
-		msg.delete();
-	}
-}
-else if(cha != msg.channel.id && msg.author.tag == "Rythm#3722") {
-	msg.delete();
-}
-else if(cha != msg.channel.id && msg.author.tag == "La3 is on Fire#5723" ) {
-	msg.delete(12000);
-}
-
-
+            var cha = msg.guild.channels.find('name', 'bot_commands').id;
+            if (cha != msg.channel.id) {
+                if (msg.content.startsWith('!')) {
+                    msg.reply('bellehi sayeb sala7 wel commands ektebhom ken fel bot_commands. ani sama7tek el marra hethi ama el marra ejjeya wlh n9oul el hama yetfehem m3ak');
+                    msg.delete();
+                } else if (msg.author.tag == "Rythm#3722") {
+                    msg.delete();
+                } else if (msg.author.tag == "La3 is on Fire#5723") {
+                    msg.delete(12000);
+                }
+            } else {
+                if (msg.content.startsWith(prefix + "pubg")) {
+                	
+                }
+            }
 
 
 
