ca10f6619d4f75377eb5321ad40ce19365a1de7d
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-04-09 16:41:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const Fortnite = require('fortnite');
+const client = new Fortnite('165f3d18-9230-4a03-89fc-9a2743e22e50');
 const bot = new Discord.Client();
 const prefix = "!";
 bot.on('ready', () => {
@@ -12,6 +14,7 @@ bot.on('ready', () => {
 
 //commands
 bot.on('message', msg => {
+	if (msg.channel.type == 'text') {
             var cha = msg.guild.channels.find('name', 'bot_commands').id;
             if (cha != msg.channel.id) {
                 if (msg.content.startsWith('!')) {
@@ -23,13 +26,38 @@ bot.on('message', msg => {
                     msg.delete(12000);
                 }
             } else {
-                if (msg.content.startsWith(prefix + "pubg")) {
-                	
+                if (msg.content.startsWith(prefix + "fortnite-pc")) {
+                	var con = msg.content.substr(0,msg.content.indexOf(' ')).split('-')[1];
+                	var name = msg.content.substr(msg.content.indexOf(' ')+1);
+                	client.getInfo(name, con).then(data => {
+                		var res = new Discord.RichEmbed();
+                		var stats = data.lifetimeStats;
+                		var tPlayed = stats.find(s => s.stat == 'timePlayed');
+                		var wins = stats.find(s => s.stat == 'wins');
+                		var win = stats.find(s => s.stat == 'win');
+                		var mPlayed = stats.find(s => s.stat == 'matchesPlayed');
+                		var kills = stats.find(s => s.stat == 'kills');
+                		var avgSurvivalTime = stats.find(s => s.stat == 'avgSurvivalTime');
+                		res
+                		.setTitle('Fortnite Stats: ' + data.username)
+                		.setThumbnail(msg.author.avatarURL)
+                		.addField("Time played", tPlayed.value)
+                		.addField("kills", kills.value)
+                		.addField("Wins", wins.value)
+                		.addField("win rate", win.value + " of " + mPlayed.value + " matches")
+                		.addField("survival time average", avgSurvivalTime.value);
+                		msg.author.send(res);
+                	}).catch(e => {
+                		console.log(e);
+                		msg.reply("fortnite bot don't know dat bitch!");
+                	});
                 }
             }
 
 
+} else {
 
+}
 });
 
 bot.login(process.env.BOT_TOKEN);
