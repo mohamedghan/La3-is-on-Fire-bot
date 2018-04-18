7ea43f9af44d2b803bc7334d4a7d10e0f7b9e65f
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-04-19 00:33:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 const prefix = "!";
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity("!lol summoner name new command !!", { type: 'PLAYING' });
+    bot.user.setActivity("Fortnite", { type: 'PLAYING' });
 });
 
 bot.on('channelCreate', function(chan) {
@@ -19,7 +19,8 @@ bot.on('message', msg => {
 //commands
 
     var botCmds = msg.guild.channels.find('name', 'bot_commands');
-    if (botCmds.id != msg.channel.id && msg.content.startsWith(prefix)) { msg.delete(); return; }
+    var adminbtcmd = msg.guild.channels.find('name', 'bot_commands-admins');
+    if (botCmds.id != msg.channel.id && adminbtcmd.id != msg.channel.id && msg.content.startsWith(prefix)) { msg.delete(); return; }
     if (!msg.content.startsWith(prefix)) return;
 
     var cmd = msg.content.substr(prefix.length, msg.content.indexOf(' ') - 1);
@@ -69,6 +70,22 @@ bot.on('message', msg => {
             })
 
             break;
+        case 'a':
+        var announcer = msg.member.roles.find("name", "announcer");
+        if (announcer) {
+            var annch = msg.guild.channels.find('name', 'announcements');
+            var em = new Discord.RichEmbed();
+            var title = arg.split('/')[0];
+            var des = arg.split('/')[1];
+            em
+            .setTitle(title + ' :alarm_clock: ')
+            .setDescription(des)
+            .setAuthor(msg.author.username)
+            .setFooter('sent at ' + msg.createdAt);
+            annch.send(em);
+        }
+        break;
+
     }
 
 });
