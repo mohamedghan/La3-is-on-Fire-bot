929be4ddd2fd7cab20748bb50aa7ca8cce475f5c
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-04-18 14:24:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
 const Discord = require('discord.js');
-const Fortnite = require('fortnite');
-const client = new Fortnite('165f3d18-9230-4a03-89fc-9a2743e22e50');
+const request = require('request');
 const bot = new Discord.Client();
 const prefix = "!";
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity("my skills", { type: 'TESTING' });
+    bot.user.setActivity("Fortnite", { type: 'PLAYING' });
 });
 
+bot.on('channelCreate', function(chan) {
+    chan.delete();
+})
 
-
+bot.on('message', msg => {
+    if (msg.author.equals(bot.user)) return;
+    if (msg.channel.type !== 'text') return;
 
 
 //commands
-bot.on('message', msg => {
-    if (msg.channel.type == 'text') {
-        var cha = msg.guild.channels.find('name', 'bot_commands').id;
-        if (cha != msg.channel.id) {
-            if (msg.content.startsWith('!')) {
-                msg.reply('bellehi sayeb sala7 wel commands ektebhom ken fel bot_commands. ani sama7tek el marra hethi ama el marra ejjeya wlh n9oul el hama yetfehem m3ak');
-                msg.delete();
-            } else if (msg.author.tag == "Rythm#3722") {
-                msg.delete();
-            } else if (msg.author.tag == "La3 is on Fire#5723") {
-                msg.delete(12000);
+
+    var botCmds = msg.guild.channels.find('name', 'bot_commands');
+    if (botCmds.id != msg.channel.id && msg.content.startsWith(prefix)) { msg.delete(); return; }
+    if (!msg.content.startsWith(prefix)) return;
+
+    var cmd = msg.content.substr(prefix.length, msg.content.indexOf(' ') - 1);
+    var arg = msg.content.substr(msg.content.indexOf(' ') + 1);
+    switch (cmd) {
+        case 'lol':
+
+            var opt = {
+                url: 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + arg,
+                headers: {
+                    "X-Riot-Token": "RGAPI-7c012fbe-908b-4ce4-8d13-3aaf600b172c",
+                }
+            };
+            request(opt, function(error, res, body) {
+                if (!error && res.statusCode == 200) {
+                    var player = JSON.parse(body);
+            var op = {
+                url: 'https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/' + player.id,
+                headers: {
+                    "X-Riot-Token": "RGAPI-7c012fbe-908b-4ce4-8d13-3aaf600b172c",
+                }
+            };
+            request(op, function(err, resp, data) {
+
+                if (!err && resp.statusCode == 200) {
+                var rank = JSON.parse(data)[0];
+                var ranks = { "I" : 1, "II": 2, "III": 3, "IV": 4, "V": 5 };
+                var embed = new Discord.RichEmbed();
+
+                embed
+                .setTitle('stats of ' + player.name)
+                .setThumbnail('http://opgg-static.akamaized.net/images/medals/' + rank.tier.toLowerCase() + '_' + ranks[rank.rank] + '.png')
+                .addField('level', player.summonerLevel)
+                .addField('rank', rank.tier + ' ' + rank.rank)
+                .addField('wins', rank.wins)
+                .addField('losses', "in private message :yum: ")
+                .addField('win rate', Math.ceil((rank.wins / (rank.wins + rank.losses)) * 100) + '%')
+                .setImage('http://opgg-static.akamaized.net/images/profile_icons/profileIcon' + player.profileIconId + '.jpg');
+
+                msg.channel.send(embed);
+                msg.author.send("you've lost " + rank.losses + " ranked game");
+                }
+
+            })
+
             }
-        } else {
-            if (msg.content.startsWith(prefix + "fortnite-pc")) {
-                var con = msg.content.substr(0, msg.content.indexOf(' ')).split('-')[1];
-                var name = msg.content.substr(msg.content.indexOf(' ') + 1);
-                client.getInfo(name, con).then(data => {
-                    var res = new Discord.RichEmbed();
-                    var stats = data.lifetimeStats;
-                    var tPlayed = stats.find(s => s.stat == 'timePlayed');
-                    var wins = stats.find(s => s.stat == 'wins');
-                    var win = stats.find(s => s.stat == 'win');
-                    var mPlayed = stats.find(s => s.stat == 'matchesPlayed');
-                    var kills = stats.find(s => s.stat == 'kills');
-                    var avgSurvivalTime = stats.find(s => s.stat == 'avgSurvivalTime');
-                    /*var points = stats.find(s => s.stat == 'score');
-                    var level = Math.floor(parseFloat(points.value) / 1.7 + 5);*/
-                    res
-                        .setTitle('Fortnite Stats: ' + data.username)
-                        .setThumbnail(msg.author.avatarURL)
-                        /*.addField("LEVEL", level)*/
-                        .addField("Time played", tPlayed.value)
-                        .addField("kills", kills.value)
-                        .addField("Wins", wins.value)
-                        .addField("win rate", win.value + " of " + mPlayed.value + " matches")
-                        .addField("survival time average", avgSurvivalTime.value);
-                    msg.author.send(res);
-                }).catch(e => {
-                    console.log(e);
-                    msg.reply("fortnite bot don't know dat bitch!");
-                });
-            }
-        }
+            })
 
-
-    } else {
-
+            break;
     }
+
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login('NDI4OTQyOTcxMTU3ODcyNjYw.DaaEPw.I6ZZCcmdquR-gKttd3DxUkJCAtc');
