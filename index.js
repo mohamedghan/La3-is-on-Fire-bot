5b21e58104b35dc985893fb92bca25f39a3c0f6c
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-11-10 19:43:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,24 @@ bot.on('message', msg => {
 
         if (msg.content == "!fuck") {
             msg.react('♥');
-            msg.reply('fuck you ' + msg.author.username + ' :D');
+           
+            https.get('https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Dg%20Firewing?api_key=RGAPI-1bdbc014-4437-4746-a22d-d3dd31daef7d', (resp) => {
+                let data = '';
+
+                 // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                 msg.reply(JSON.parse(data).explanation);
+                 });
+
+                 }).on("error", (err) => {
+                    console.log("Error: " + err.message);
+                 });
+            
         }
     
     
