f3cd6029188dd8d8702852f506e9fdbeab1909a1
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-11-11 00:03:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const Discord = require('discord.js');
 const https = require('https');
-const request = require('request');
 const bot = new Discord.Client();
-const prefix = "!";
+const prefix = {
+    lol:"!lol/"
+};
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity("Fortnite", { type: 'PLAYING' });
+    bot.user.setActivity(`${prefix.lol}summoner name|Euw Only`, { type: 'PLAYING' });
 });
 
 bot.on('message', msg => {
 
     if (msg.author.equals(bot.user)) return;
-    if (!msg.content.startsWith(prefix)) return;
-
-//commands
+    if (msg.content.startsWith(prefix.lol)) {
 
 
-
-            msg.react('♥');
-           
-            https.get('https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + msg.content.replace('!','') + '?api_key=RGAPI-1bdbc014-4437-4746-a22d-d3dd31daef7d', (resp) => {
+            let sname = msg.content.replace(prefix.lol,'');
+            if (sname == "") return;
+            https.get(`https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${sname}?api_key=RGAPI-1bdbc014-4437-4746-a22d-d3dd31daef7d`, (resp) => {
                 let data = '';
-
-                 // A chunk of data has been recieved.
                 resp.on('data', (chunk) => {
                     data += chunk;
                 });
 
-                // The whole response has been received. Print out the result.
                 resp.on('end', () => {
-                 let base = JSON.parse(data);
-                 msg.reply(base.name + ' is a level '+ base.summonerLevel + ' boosted guy, report him and makee your life so much !');
-                 });
+                    let base = JSON.parse(data);
+                    if (base.status != null) {
+                        msg.react('😫');
+                    } else {
+                    msg.react('♥');
+                    msg.reply(`${base.name} is a level ${base.summonerLevel} boosted guy, report him and make your life much more easier!`);
+                    }
+                });
 
-                 }).on("error", (err) => {
+                }).on("error", (err) => {
                     console.log("Error: " + err.message);
-                 });
+                    msg.react('😫');
+                });
             
-    
+    }
     
 });   
 
