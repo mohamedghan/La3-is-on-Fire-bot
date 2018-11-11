5ec6c33dd0733eb16f182e78d8c7e68f94c13bbc
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-11-11 15:10:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,27 @@ bot.on('message', msg => {
                     if (base.status != null) {
                         msg.react('😫');
                     } else {
-                    msg.react('♥');
-                    msg.reply(`${base.name} is a level ${base.summonerLevel} boosted guy, report him and make your life much more easier!`);
+                    https.get(`https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/${base.id}?api_key=RGAPI-1bdbc014-4437-4746-a22d-d3dd31daef7d`, (respa) => {
+                    let dataa = '';
+                    respa.on('data', (chunka) => {
+                        dataa += chunka;
+                    });
+
+                    respa.on('end', () => {
+                    let base1 = JSON.parse(dataa);
+                    if (base1.status != null) {
+                        msg.react('😫');
+                    } else {
+                        let msg = `${base.name} is a level ${base.summonerLevel} and ${base1[0].tier} ${base1[0].rank}(${base1[0].leaguePoints}LP) boosted guy, report him and make your life much more easier!`;
+                        msg.react('♥');
+                        msg.reply(msg);
+                        }
+                    });
+
+                    }).on("error", (err) => {
+                        console.log("Error: " + err.message);
+                        msg.react('😫');
+                    });
                     }
                 });
 
