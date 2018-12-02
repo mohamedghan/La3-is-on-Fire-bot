d6b2625ef4e21c9e2e383e356f4973a0a6c116ad
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2018-12-02 23:43:17 +0100

    Add files via upload

---
+++ cmds/rainbow.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+
+module.exports.run = async (bot, msg, args, con) => {
+	    data = args.join();
+	    let text = data.split(';')[0];
+	    let link = data.split(';')[1];
+        var hooka = new Discord.WebhookClient(con.webhooks.rainbow.id, con.webhooks.rainbow.token);
+
+        hooka.send(text, {
+            "file" : link
+        })
+}
+
+module.exports.help = {
+	name: "r6"
+}
